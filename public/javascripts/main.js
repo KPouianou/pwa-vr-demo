c783898446f951feb7e6648b8ce7f967af0fb6dd
Pouianou <kpouianou@gmail.com>
2019-01-07 13:37:05 +0200

    Use WebVR sphere instead of standard 3D one

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,23 +4,53 @@ var canvas = document.getElementById("renderCanvas");
 // Generate the BABYLON 3D engine
 var engine = new BABYLON.Engine(canvas, true);
 
+/**
+ * Standard 3D sphere
+ */
+// var createScene = function () {
+
+//     // Create the scene space
+//     var scene = new BABYLON.Scene(engine);
+
+//     // Add a camera to the scene and attach it to the canvas
+//     var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
+//     camera.attachControl(canvas, true);
+
+//     // Add lights to the scene
+//     var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+//     var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+
+//     // This is where you create and manipulate meshes
+//     var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene);
+
+//     return scene;
+// };
+
+/**
+ * WebVR 3d Sphere
+ */
 var createScene = function () {
 
-    // Create the scene space
-    var scene = new BABYLON.Scene(engine);
+  // Create the scene space
+  var scene = new BABYLON.Scene(engine);
 
-    // Add a camera to the scene and attach it to the canvas
-    var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
+  // Add a camera to the scene and attach it to the canvas
+  var camera = new BABYLON.WebVRFreeCamera("Camera", new BABYLON.Vector3(0, 0, 0), scene);
 
-    // Add lights to the scene
-    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-    var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+  scene.onPointerDown = function () {
+      scene.onPointerDown = undefined;
+      camera.attachControl(canvas, true);
+  };
 
-    // This is where you create and manipulate meshes
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene);
+  // Add lights to the scene
+  var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+  var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
-    return scene;
+  // This is where you create and manipulate meshes
+  var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene);
+  sphere.position.z = 10;
+
+  return scene;
 };
 
 var scene = createScene(); //Call the createScene function
